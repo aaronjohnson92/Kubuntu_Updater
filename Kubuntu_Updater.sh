2e4a987a58b2aa7cfe9ab32cbb9fd6049ce48079
aaronjohnson92 <38840150+aaronjohnson92@users.noreply.github.com>
2018-05-16 09:52:08 -0400

    Add files via upload

--- Kubuntu_Updater.sh
+++ Kubuntu_Updater.sh
@@ -4,6 +4,8 @@ sudo add-apt-repository ppa:webupd8team/java
 sudo apt-get update 
 sudo apt-get install oracle-java8-installer
 
+sudo adduser `id -un` libvirtd
+
 sudo sh -c 'curl https://dl-ssl.google.com/linux/linux_signing_key.pub | apt-key add -'
 sudo sh -c 'curl https://storage.googleapis.com/download.dartlang.org/linux/debian/dart_stable.list > /etc/apt/sources.list.d/dart_stable.list'
 
